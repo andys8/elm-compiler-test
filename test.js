0bf3c5e2cbce4c7a80aaf389fe6d5317bc296cb6
andys8 <andys8@users.noreply.github.com>
2019-07-12 02:22:58 +0200

    Test for specific binary version

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const tmp = require("tmp-promise");
 const { spawn } = require("child_process");
 const packages = require("./search.json");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const packageNames = packages.map(({ name }) => name);
 
@@ -8,41 +10,50 @@ tmp.setGracefulCleanup();
 
 const elmBin = __dirname + "/bin/elm-0.19.1-alpha-1";
 
-describe.each(packageNames.map(x => [x]))("Package %s", packageName => {
-  test(
-    "elm install",
-    async done => {
-      console.debug(packageName);
+describe("Elm compiler test", () => {
 
-      let err = "";
-      const dir = await tmp.dir({ unsafeCleanup: true });
+  test("Binary has expected version", async () => {
+    const { stdout } = await exec(`${elmBin} --version`);
+    console.debug(`Elm version ${stdout}`);
+    expect(stdout).toEqual("0.19.1\n");
+  });
 
-      const elmInit = spawn(elmBin, ["init"], { cwd: dir.path });
-      elmInit.stderr.on("data", data => (err += data.toString()));
-      elmInit.stdout.on("data", () => {
-        elmInit.stdin.write("y\n");
-      });
+  describe.each(packageNames.map(x => [x]))("Package %s", packageName => {
+    test(
+      "elm install",
+      async done => {
+        console.debug(packageName);
 
-      elmInit.on("exit", code => {
-        expect(err).toEqual("");
-        expect(code).toEqual(0);
+        let err = "";
+        const dir = await tmp.dir({ unsafeCleanup: true });
 
-        const elmInstall = spawn(elmBin, ["install", packageName], {
-          cwd: dir.path
-        });
-        elmInstall.stderr.on("data", data => (err += data.toString()));
-        elmInstall.stdout.on("data", () => {
-          elmInstall.stdin.write("y\n");
+        const elmInit = spawn(elmBin, ["init"], { cwd: dir.path });
+        elmInit.stderr.on("data", data => (err += data.toString()));
+        elmInit.stdout.on("data", () => {
+          elmInit.stdin.write("y\n");
         });
 
-        elmInstall.on("exit", async code => {
+        elmInit.on("exit", code => {
           expect(err).toEqual("");
           expect(code).toEqual(0);
-          await dir.cleanup();
-          done();
+
+          const elmInstall = spawn(elmBin, ["install", packageName], {
+            cwd: dir.path
+          });
+          elmInstall.stderr.on("data", data => (err += data.toString()));
+          elmInstall.stdout.on("data", () => {
+            elmInstall.stdin.write("y\n");
+          });
+
+          elmInstall.on("exit", async code => {
+            expect(err).toEqual("");
+            expect(code).toEqual(0);
+            await dir.cleanup();
+            done();
+          });
         });
-      });
-    },
-    60 * 1000
-  );
+      },
+      60 * 1000
+    );
+  });
 });
